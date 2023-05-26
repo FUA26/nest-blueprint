87ab09adfab573194d6e5fc2fbe7e56608dd8a00
Fatih <Fatihulil@gmail.com>
2023-05-27 02:03:46 +0700

    config type orm with sqlite3 database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import appConfig from './configs/app.config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: ['dist/**/*.entity.js'],
+      synchronize: true,
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       load: [appConfig],
