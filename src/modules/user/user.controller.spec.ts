1f2213f06ebeb019846fd7da74778d788bd0fd9e
Fatih <Fatihulil@gmail.com>
2023-05-27 23:15:57 +0700

    create unit test

--- src/modules/user/user.controller.spec.ts
+++ src/modules/user/user.controller.spec.ts
@@ -5,16 +5,146 @@ import { UserService } from './user.service';
 describe('UserController', () => {
   let controller: UserController;
 
+  const mockUserService = {
+    create: jest.fn((dto) => {
+      return {
+        id: '1',
+        ...dto,
+      };
+    }),
+    findAll: jest.fn(() => {
+      return [
+        {
+          id: '1',
+          firstName: 'John',
+          lastName: 'Doe',
+          isActive: true,
+        },
+        {
+          id: '2',
+          firstName: 'Jane',
+          lastName: 'Smith',
+          isActive: false,
+        },
+      ];
+    }),
+    findOne: jest.fn((id) => {
+      return {
+        id,
+        firstName: 'John',
+        lastName: 'Doe',
+        isActive: true,
+      };
+    }),
+    update: jest.fn((id, dto) => {
+      return {
+        id,
+        ...dto,
+      };
+    }),
+    remove: jest.fn((id) => {
+      return {
+        id,
+        deleted: true,
+      };
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
       providers: [UserService],
-    }).compile();
+    })
+      .overrideProvider(UserService)
+      .useValue(mockUserService)
+      .compile();
 
     controller = module.get<UserController>(UserController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('Create', () => {
+    it('should create a new User', () => {
+      const dto = {
+        firstName: 'Jose',
+        lastName: 'deamore',
+        isActive: false,
+      };
+      expect(controller.create(dto)).toEqual({
+        id: expect.any(String),
+        ...dto,
+      });
+      expect(mockUserService.create).toHaveBeenCalledWith(dto);
+    });
+  });
+
+  describe('FindAll', () => {
+    it('should return an array of Users', () => {
+      expect(controller.findAll()).toEqual([
+        {
+          id: '1',
+          firstName: 'John',
+          lastName: 'Doe',
+          isActive: true,
+        },
+        {
+          id: '2',
+          firstName: 'Jane',
+          lastName: 'Smith',
+          isActive: false,
+        },
+      ]);
+      expect(mockUserService.findAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('FindOne', () => {
+    it('should return a User by ID', () => {
+      const userId = '1';
+      expect(controller.findOne(userId)).toEqual({
+        id: 1,
+        firstName: 'John',
+        lastName: 'Doe',
+        isActive: true,
+      });
+      expect(mockUserService.findOne).toHaveBeenCalledWith(+userId);
+    });
+  });
+
+  describe('Update', () => {
+    it('should update a User', () => {
+      const userId = '1';
+      expect(
+        controller.update(userId, {
+          firstName: 'Updated',
+          lastName: 'User',
+          isActive: false,
+        }),
+      ).toEqual({
+        id: 1,
+        firstName: 'Updated',
+        lastName: 'User',
+        isActive: false,
+      });
+      expect(mockUserService.update).toHaveBeenCalledWith(+userId, {
+        firstName: 'Updated',
+        lastName: 'User',
+        isActive: false,
+      });
+    });
+  });
+
+  describe('Remove', () => {
+    it('should remove a User', () => {
+      const userId = '1';
+      expect(controller.remove(userId)).toEqual({
+        id: 1,
+        deleted: true,
+      });
+      expect(mockUserService.remove).toHaveBeenCalledWith(+userId);
+    });
   });
 });
